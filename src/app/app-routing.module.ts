f2563d04ec27fbbc7a066eb1ff789c3795316f20
AgusJems <59831192+AgusJems@users.noreply.github.com>
2023-06-19 10:25:19 +0700

    first

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,39 @@
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { AppLayoutComponent } from './layout/app.layout.component';
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(
+            [
+                {
+                    path: '',
+                    component: AppLayoutComponent,
+                    children: [
+                        {
+                            path: '',
+                            loadChildren: () =>
+                                import(
+                                    './demo/components/dashboard/dashboard.module'
+                                ).then((m) => m.DashboardModule),
+                        },
+                        {
+                            path: 'pages',
+                            loadChildren: () =>
+                                import(
+                                    './demo/components/pages/pages.module'
+                                ).then((m) => m.PagesModule),
+                        },
+                    ],
+                },
+            ],
+            {
+                scrollPositionRestoration: 'enabled',
+                anchorScrolling: 'enabled',
+                onSameUrlNavigation: 'reload',
+            }
+        ),
+    ],
+    exports: [RouterModule],
+})
+export class AppRoutingModule {}
